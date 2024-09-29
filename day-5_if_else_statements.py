f1d5d939d825e04895cb7acabe9dbbbef9305abc
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-09-29 10:06:05 +0545

    day5

---
+++ day-5_if_else_statements.py
@@ -0,0 +1,20 @@
+x=int(input("what is your age? "))
+if x>=18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+
+y=str(input("enter a name:"))
+
+if len(y)>= 5:
+    print("your name is long")
+else:
+    print("your name is short")
+
+MyName=str(input("enter a name"))
+
+if MyName == "indira" :
+    print("welcome user")
+else:
+    print("welcome", MyName)
