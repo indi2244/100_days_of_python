edc5193a98ca62a6cac3c4ae63764cdbec5204d0
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-09-29 09:42:28 +0545

    day3

--- day-3_concatenation.py
+++ day-3_concatenation.py
@@ -6,3 +6,8 @@ print("hello",my_name,"you are",my_age,"years old")
 my_name=input("what is your name")
 my_age=input("what is your age")
 print("hello "+ my_name +" you are "+ my_age +" years old") # comma or plus is used for concatenation
+
+
+school=input("which school do you go to ? ")
+subject=input("which is your major subject? ")
+print("i go to ", school ,"and my major is " , subject )
