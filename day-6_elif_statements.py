f852c66e57140b48bf428af07f827b35ac8d32af
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-09-29 10:40:17 +0545

    day5

---
+++ day-6_elif_statements.py
@@ -0,0 +1,46 @@
+ingredient=input("name an ingredient:")
+
+if ingredient== 'vegetable':
+    print("lets make vegetale soup")
+
+elif ingredient == "chicken":
+    print("lets make chicken soup")
+
+elif ingredient == "mushroom":
+    print("lets make mushroom soup")
+
+else:
+    print("lets eat out")
+
+
+is_male=False
+is_tall=True
+
+if is_male== True and is_tall==True:
+    print("you are a tall male")
+
+elif is_male== True and is_tall==False:
+    print("you are a short male")
+
+elif is_male== False and is_tall==False:
+    print("you are a short female")
+
+else:
+    print("you are a tall female")
+
+
+
+is_male=str(input("You are a male?"))
+is_tall=str(input("you are tall?"))
+
+if is_male== True and is_tall==True:
+    print("you are a tall male")
+
+elif is_male== True and is_tall==False:
+    print("you are a short male")
+
+elif is_male== False and is_tall==False:
+    print("you are a short female")
+
+else:
+    print("you are a tall female")
