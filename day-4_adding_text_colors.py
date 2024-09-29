b0b7aec94e008bdca6e9a14ba0a2f445707cf1a4
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-09-29 09:54:29 +0545

    day4

---
+++ day-4_adding_text_colors.py
@@ -0,0 +1,3 @@
+hero=input("which hero do you like? ")
+villain=input("name a villain? ")
+print("i like  ", hero ,"and the villain is " , villain ,"\033[35m", " The battle between them is very ","\033[35m","\033[31m" ,"interesting" ,"\033[31m" )
