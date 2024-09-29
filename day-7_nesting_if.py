f9933362fe12f1aeebf7490df5467c69c7616190
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-09-29 10:56:06 +0545

    day7

---
+++ day-7_nesting_if.py
@@ -0,0 +1,14 @@
+flower=input("enter your favourite flower:")
+
+if flower== "rose":
+    print("yeah mine's too")
+    color= input("which colored rose do you like?")
+    if color == 'pink':
+        print("yeah that's beautiful")
+    else:
+        print("i prefer pink")
+elif flower == "sunflower":
+    print("yeah! that's beautiful") 
+
+else:
+    print("i prefer roses over any other flower")
